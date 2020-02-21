39d188a0a17caa75d098b5fb5f2bbf33d0ce1920
Yiyun Liu <liuyiyun@terpmail.umd.edu>
2020-02-21 16:27:09 -0500

    add lawsconcat, sconcat

--- liquid-base/src/Data/Semigroup.hs
+++ liquid-base/src/Data/Semigroup.hs
@@ -4,19 +4,28 @@ module Data.Semigroup where
 
 import           Prelude                 hiding ( Semigroup(..)
                                                 , Monoid(..)
+
                                                 )
+
+data VList a = VNil | VCons a (VList a)
 
 class Semigroup a where
     mappend :: a -> a -> a
     {-@ lawAssociative :: v:a -> v':a -> v'':a -> {mappend (mappend v v') v'' == mappend v (mappend v' v'')} @-}
+    sconcat :: a -> VList a -> a
     lawAssociative :: a -> a -> a -> ()
-
+    {-@ lawSconcat :: x:a -> ys:VList a -> {vfoldr mappend x ys == sconcat x ys} @-}
+    lawSconcat :: a -> VList a -> ()
 
 class Semigroup a => Monoid a where
    mempty :: a
    {-@ lawEmpty :: x:a -> {mappend x mempty == x && mappend mempty x == x} @-}
    lawEmpty :: a -> ()
 
+{-@ reflect vfoldr @-}
+vfoldr :: (a -> b -> b) -> b -> VList a -> b
+vfoldr _ x VNil = x
+vfoldr f x (VCons y ys) = vfoldr f (f y x) ys
 
 -- Natural Numbers
 data PNat = Z | S PNat
@@ -26,6 +35,8 @@ instance Semigroup PNat where
   mappend (S m) n = S (mappend m n)
   lawAssociative Z     _ _ = ()
   lawAssociative (S p) m n = lawAssociative p m n
+  sconcat = vfoldr mappend
+  lawSconcat _ _ = ()
 
 instance Monoid PNat where
   mempty = Z
