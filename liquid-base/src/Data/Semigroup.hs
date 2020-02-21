d2ddd8190ef8da7ca5957cf7e25b184a658aaa15
Yiyun Liu <liuyiyun@terpmail.umd.edu>
2020-02-21 15:15:09 -0500

    add semigroup with natural number instance

--- liquid-base/src/Data/Semigroup.hs
+++ liquid-base/src/Data/Semigroup.hs
@@ -1,3 +1,33 @@
-
+{-@ LIQUID "--reflection" @-}
+{-@ LIQUID "--ple" @-}
 module Data.Semigroup where
 
+import           Prelude                 hiding ( Semigroup(..)
+                                                , Monoid(..)
+                                                )
+
+class Semigroup a where
+    mappend :: a -> a -> a
+    {-@ lawAssociative :: v:a -> v':a -> v'':a -> {mappend (mappend v v') v'' == mappend v (mappend v' v'')} @-}
+    lawAssociative :: a -> a -> a -> ()
+
+
+class Semigroup a => Monoid a where
+   mempty :: a
+   {-@ lawEmpty :: x:a -> {mappend x mempty == x && mappend mempty x == x} @-}
+   lawEmpty :: a -> ()
+
+
+-- Natural Numbers
+data PNat = Z | S PNat
+
+instance Semigroup PNat where
+  mappend Z     n = n
+  mappend (S m) n = S (mappend m n)
+  lawAssociative Z     _ _ = ()
+  lawAssociative (S p) m n = lawAssociative p m n
+
+instance Monoid PNat where
+  mempty = Z
+  lawEmpty Z     = ()
+  lawEmpty (S m) = lawEmpty m
