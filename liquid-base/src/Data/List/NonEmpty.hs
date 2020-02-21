bf2d326ed820ec133cb5943ac2ddb038596c4230
James Parker <dev@jamesparker.me>
2020-02-21 17:27:11 -0500

    NonEmpty breaks semigroup

--- liquid-base/src/Data/List/NonEmpty.hs
+++ liquid-base/src/Data/List/NonEmpty.hs
@@ -8,3 +8,11 @@ import           Prelude                 hiding ( foldr
 import           Data.List
 
 data NonEmpty a = NonEmpty a (List a)
+
+{-@ reflect head @-}
+head :: NonEmpty a -> a
+head (NonEmpty a _) = a
+
+{-@ reflect tail @-}
+tail :: NonEmpty a -> List a
+tail (NonEmpty _ t) = t
