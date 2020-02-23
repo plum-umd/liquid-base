ca1f61b86488eaeaee9eb6a7029d4776d78a537f
James Parker <dev@jamesparker.me>
2020-02-22 22:30:24 -0500

    bug with lawApplicativeComposition?

--- liquid-base/src/Data/Functor.hs
+++ liquid-base/src/Data/Functor.hs
@@ -25,20 +25,27 @@ class Functor m => VFunctor m where
     lawFunctorId :: m a -> ()
 
 --     TODO: This doesn't unify XXX
-    {-@ lawFunctorComposition :: forall a b c . f:(b -> c) -> g:(a -> b) -> x:m a -> { fmap (compose f g) x == compose (fmap f) (fmap g) x } @-}
-    lawFunctorComposition :: (b -> c) -> (a -> b) -> m a -> ()
+--     {-@ lawFunctorComposition :: forall a b c . f:(b -> c) -> g:(a -> b) -> x:m a -> { fmap (compose f g) x == compose (fmap f) (fmap g) x } @-}
+--     lawFunctorComposition :: (b -> c) -> (a -> b) -> m a -> ()
 
 class Functor f => Applicative f where
   {-@ pure :: forall a. a -> f a @-}
   pure :: a -> f a
   {-@ ap :: forall a b. f (a -> b) -> f a -> f b @-}
   ap :: f (a -> b) -> f a -> f b
+  liftA2 :: (a -> b -> c) -> f a -> f b -> f c
+  (*>) :: f a -> f b -> f b
+  (<*) :: f a -> f b -> f a
 
-  -- JP: There are others here, but I don't think they're necessary
 
 class (Functor f, Applicative f) => VApplicative f where
-  {-@ myprop :: forall a b. x:f a -> f:(a -> b) -> {fmap f x == ap (pure f) x} @-}
-  myprop :: f a -> (a -> b) -> ()
+  {-@ lawApplicativeId :: forall a . v:f a -> {ap (pure id') v = v} @-}
+  lawApplicativeId :: f a -> ()
+  {-@ lawApplicativeComposition :: forall a b c . u:f (b -> c) -> v:f (a -> b) -> w:f a -> {ap (ap (ap (pure compose) u) v) w = ap u (ap v w)} @-}
+  lawApplicativeComposition :: f (b -> c) -> f (a -> b) -> f a -> ()
+
+  -- {-@ myprop :: forall a b . x:f a -> f:(a -> b) -> {fmap f x == ap (pure f) x} @-}
+  -- myprop :: f a -> (a -> b) -> ()
 
   
 {-@ data MyId a = MyId a @-}
@@ -51,10 +58,16 @@ instance Functor MyId where
 instance Applicative MyId where
   pure = MyId
   ap (MyId f) (MyId a) = MyId (f a)
+  liftA2 f (MyId a) (MyId b) = MyId (f a b)
+  a1 *> a2 = ap (id' <$ a1) a2
+  a1 <* a2 = liftA2 (\x _ -> x) a1 a2
 
 instance VApplicative MyId where
-  myprop _ _ = ()
+  lawApplicativeId _ = ()
 
+  -- myprop _ _ = ()
+
+-- TODO: Define `Maybe a` in Data.Maybe
 data Optional a = None | Has a
 
 instance Functor Optional where
@@ -68,9 +81,15 @@ instance Applicative Optional where
   ap None _ = None
   ap _ None = None
   ap (Has f) (Has x) = Has (f x)
+  liftA2 f (Has a) (Has b) = Has (f a b)
+  liftA2 f _       _       = None
+  a1 *> a2 = ap (id' <$ a1) a2
+  a1 <* a2 = liftA2 (\x _ -> x) a1 a2
 
 instance VApplicative Optional where
-  myprop _ _ = ()
+  lawApplicativeId _ = undefined -- TODO: FIXME XXX
+
+  -- myprop _ _ = ()
 
 {-@ impl :: x:Bool -> y:Bool -> {v:Bool | v <=> (x => y)} @-}
 impl :: Bool -> Bool -> Bool
