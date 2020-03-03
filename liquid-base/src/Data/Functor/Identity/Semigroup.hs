32d0b1db3abbd6ef0c8493593acca622760e7247
James Parker <dev@jamesparker.me>
2020-03-02 21:55:04 -0500

    Identity VMonoid

--- liquid-base/src/Data/Functor/Identity/Semigroup.hs
+++ liquid-base/src/Data/Functor/Identity/Semigroup.hs
@@ -19,6 +19,23 @@ import           Prelude                 hiding ( Functor(..)
 import           Liquid.ProofCombinators
 import Data.Function
 import Data.Functor.Identity
+import Data.List
+import Data.List.NonEmpty
 import Data.Semigroup.Classes
 
--- todo?
+instance Semigroup a => Semigroup (Identity a) where
+  mappend (Identity v) (Identity v') = Identity (mappend v v')
+  sconcat (NonEmpty h t) = foldlList mappend h t
+
+instance Monoid a => Monoid (Identity a) where
+  mempty = Identity mempty
+  mconcat xs = foldrList mappend mempty xs
+
+instance VSemigroup a => VSemigroup (Identity a) where
+  lawAssociative (Identity v) (Identity v') (Identity v'') = lawAssociative v v' v''
+  lawSconcat (NonEmpty h t) = sconcat (NonEmpty h t) `cast` ()
+
+instance VMonoid a => VMonoid (Identity a) where
+  lawEmpty (Identity v) = lawEmpty v
+  lawMconcat xs = mconcat xs `cast` ()
+
